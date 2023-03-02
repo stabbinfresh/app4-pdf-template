abc5987c7878ad9dc3f948b960331e2878306487
Andrew Kuhs <andythekuhs@gmail.com>
2023-03-02 18:49:57 -0500

    for loop pdf generation

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from fpdf import FPDF
+import pandas as pd
 
+
+df = pd.read_csv("topics.csv")
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-pdf.add_page()
-
-pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hi There!", align="L", ln=1)
-pdf.set_font(family="Times", size=10)
-pdf.cell(w=0, h=12, txt="Hello There!", align="L", ln=1)
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    pdf.line(10, 21, 200, 21)
 
 pdf.output("output.pdf")
